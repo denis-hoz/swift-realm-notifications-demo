baa910c29270b838293b63dcc7b7eaacc34b5029
Denis Kapusta <dkapusta@scnsoft.com>
2016-06-08 13:16:01 +0300

    Add repo object mapping

--- swift-realm-notifications-demo/Entity/Repo/DKRepo.swift
+++ swift-realm-notifications-demo/Entity/Repo/DKRepo.swift
@@ -5,14 +5,27 @@
 
 import Foundation
 import RealmSwift
+import ObjectMapper
 
-class DKRepo : Object {
-    //MARK: properties
+class DKRepo : Object, Mappable {
+    // MARK: properties
     dynamic var name = ""
     dynamic var id: Int = 0
     dynamic var stars = 0
-    dynamic var pushedAt: NSTimeInterval = 0
+    dynamic var pushedAt: NSDate = NSDate(timeIntervalSince1970: 0)
 
-    //MARK: meta
+    // MARK: meta
 	override class func primaryKey() -> String? { return "id" }
+    
+    // MARK: Mappable
+    convenience required init?(_ map: Map) {
+        self.init()
+    }
+
+    func mapping(map: Map) {
+        name <- map["name"]
+        id <- map["id"]
+        stars <- map["stargazers_count"]
+        pushedAt <- (map["pushed_at"], DateTransform())
+    }
 }
